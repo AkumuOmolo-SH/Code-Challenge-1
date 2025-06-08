23e50f05863046472d2c3481a0e1c3c5cd3091ec
AkumuOmolo <akumu.omoloh@gmail.com>
2025-06-08 16:40:05 +0300

    Adds chai.js

--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -1,29 +1,26 @@
 const oneCupRecipe = {
-    Water = 200,
-    Milk = 50,
-    TeaLeaves = 1,
-    Sugar = 2
+    Water = 200, //ml
+    Milk = 50,  //ml
+    TeaLeaves = 1, //tablespoon
+    Sugar = 2  //tablespoon
 }
 
+function calculateChaiIngredients(numberOfCups) { //calculates total ingredients based on no. of cups
+    const recipe = {};//stores total amounts
 
-function calculateChaiIngredients(numberOfCups) {
-    const recipe = {};
-
-for (const key in oneCupRecipe) {
-    recipe(oneCupRecipe[key] * numberOfCups);
+for (const key in oneCupRecipe) { //Loops through the ingredients in the oneCupRecipe
+    recipe[key]=(oneCupRecipe[key] * numberOfCups); 
 
 } 
+//Shows results in a clear, readbale format
 console.log(`To make ${numberofCups} of Kenyan Chai, you will need:`);
 console.log(`Water: &{recipe.Water} ml`);
 console.log(`Milk: &{recipe.Milk} ml`);
 console.log(`Tea Leaves (Majani): &{recipe.TeaLeaves} ml`);
 }
 
-
-
-
-const cups = prompt("Karibu! How many cups of Chai Bora would you like to make?");
-const numberOfCups = parseInt(cups);
+const cups = prompt("Karibu! How many cups of Chai Bora would you like to make?");// Prompts the user to input how many cups
+const numberOfCups = parseInt(cups); //converts input to number.
 
 
 
