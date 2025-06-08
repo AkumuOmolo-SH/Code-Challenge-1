9eb4ef2bcb957bbd0292b3b05fa72a2faaefa935
AkumuOmolo <akumu.omoloh@gmail.com>
2025-06-08 18:27:01 +0300

    Adds fee.js

---
+++ mobileMoneyEstimator/fee.js
@@ -0,0 +1,26 @@
+const basicFee = 0.015 //basic transaction fee os 1.5% of the transaction amount
+
+function estimateTransactionFee(amountToSend) {
+    const calculatedFee = amountToSend * basicFee;
+    let fee;
+
+    if (calculatedFeeFee < 10) {
+        fee = 10; //Minimum fee 
+    } else if (calculatedFee > 70) {
+        fee = 70; //Maximum fee
+    } else {
+        fee = calculatedFee; 
+    }
+
+    const total = amountToSend + fee; //Calculates total amount that will be debited
+
+    //Output the result
+    console.log(`Sending KES ${amountToSend}:`);
+    console.log(`Calculated Transaction Fee: KES ${fee}`);
+    console.log(`Total amount to be debited: KES ${total}`);
+    console.log("\nSend Money Securely!");   
+}
+
+//Prompt user to enter an amount
+const input = prompt("Unatuma Ngapi? (KES):");
+const amountToSend = parseFloat(input);
