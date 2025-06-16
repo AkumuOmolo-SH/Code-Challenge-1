f72bcb7291a3ad02fcd37419d7d0dfd031eddf63
AkumuOmolo <akumu.omoloh@gmail.com>
2025-06-16 15:14:31 +0300

    Adds fare.js chai.js fee.js

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,16 +1,22 @@
 function calculateBodaFare (distanceInKm) {
     const baseFare = 50; //Base Fare in Kes
     const chargeperKm = 15; // charges per Km in Kes
-    const totalFare = baseFare + (distanceInKm * chargeperKm); //calculates total fare estimate
-
+    const distanceFare = distanceInKm * chargeperKm
+    const totalFare = baseFare + distanceFare; 
+    
     //Displays fare breakdown
     console.log(`Uko kwote? Io ni ${distanceInKm} km`);
     console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
     console.log(`Mpaka Uko: KES ${distanceFare}`);
     console.log(`Total: KES ${totalFare}`);
     console.log("Panda PikiPiki!");
+}
 
-    const input = prompt("Unafika wapi Mkubwa? Kilometer ngapi?":); //Propmts user to enter distance in Kilometers
-    const distanceInKm = parseFloat(input); //Converts input to a number
+    const input = prompt("Unafika wapi Mkubwa? Kilometer ngapi?"); //Prompts user to enter distance in Kilometers
+    const distance = parseFloat(input); //Converts input to a number
 
+if (distance >= 0) {
+  calculateBodaFare(distance);
+} else {
+  console.log("Tafadhali andika nambari");
 }
